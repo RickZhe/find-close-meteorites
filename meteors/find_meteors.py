947c852d32c02ea6d8a1b106d646aa3aa46da3ff
Rick Zheng <rzheng@presidio.com>
2018-12-04 00:22:34 -0600

    modify the __name__ = '__main__'

--- meteors/find_meteors.py
+++ meteors/find_meteors.py
@@ -26,9 +26,9 @@ if __name__ == '__main__':
     for meteor in meteor_data:
         if not ('reclat' in meteor and 'reclong' in meteor): continue
         meteor['distance'] = calc_dist(float(meteor['reclat']),
-                                       float(meteor['reclong']),
-                                       my_loc[0],
-                                       my_loc[1])
+                                    float(meteor['reclong']),
+                                    my_loc[0],
+                                    my_loc[1])
 
     meteor_data.sort(key=get_dist)
 
